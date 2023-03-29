258fa984ca7e388f8e30eec0b2532e3d2622cc0f
Eccirillo69 <ettore.cirillo.dev@gmail.com>
2023-03-29 15:49:46 +0200

    Implementazione del routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SearchComponent } from './components/search/search.component';
-import { SinglePokemonComponent } from './components/single-pokemon/single-pokemon.component';
 
 const routes: Routes = [
   {path: "", pathMatch:"full", redirectTo: "/home"},
   {path: "home", component: HomeComponent},
-  {path: "search", component: SearchComponent, children: [
-    {path: ":id", component: SinglePokemonComponent },
-  ]},
+  {path: "search", component: SearchComponent},
   {path: "404", component: NotFoundComponent},
   {path: "**", redirectTo: "/404"}
 ];
